bab4a6c88d5536991f616890448901b4d8f32499
Gourav Paul <gourav26061991@gmail.com>
2019-04-29 19:23:36 +0530

    msg

--- src/main/java/com/unw/service/HelloWorldService.java
+++ src/main/java/com/unw/service/HelloWorldService.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Component;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
-import org.springframework.boot.info.BuildProperties;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author UNGERW
@@ -14,8 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Component
 @Path("/spring-docker/")
 public class HelloWorldService {
-    @Autowired
-    BuildProperties buildProperties;
 
     @GET
     @Path("/hello")
